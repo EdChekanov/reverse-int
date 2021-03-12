730d8ec3a6790c0058fbb3ed20e8b16c58d674c7
EdChekanov <kide.da@gmail.com>
2021-03-13 00:36:29 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+    n = String(n);
+    if (n[0] == '-') {
+      n = n.slice(1);
+    }
+    let result = '';
+    let i = n.length - 1;
+    while (i >= 0) {
+        result = result + n[i];
+        i--;
+    }
+    return result;
+  }
